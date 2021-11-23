1b6a2cb09d7fa4d970b40fca46b6abc7e8908bc6
Nathan <nathanocampo7@gmail.com>
2021-11-22 20:56:01 -0500

    fixed excel graph ranges and flipped y values for freq and strain graphs

--- Heart Beating Analysis.py
+++ Heart Beating Analysis.py
@@ -12,10 +12,10 @@ import numpy as np
 import os
 import pandas as pd
 
-timepoints = 4 #input('How many timepoints: ')
+timepoints = 3#input('How many timepoints: ')
 tps = []
-treatment = 'ctrl' #string.empty
-con = "collagenase" #input('Enter Conditon: ')
+treatment = 'trtd' #string.empty
+con = "media" #input('Enter Conditon: ')
 
 # if(input('Treatment or Control (t/c):')== 't'):
 #      treatment = 'trtd'
@@ -26,18 +26,18 @@ con = "collagenase" #input('Enter Conditon: ')
 
 hrt = input('Enter Sample #: ')
 
-if(timepoints == 4):
+if(timepoints == 3):
+    tps = [0, 420, 1440]
+
+elif(timepoints == 4):
     tps = [0, 15, 30, 45]
 
-elif(timepoints == 3):
-    tps = [0, 57600, 144000]
-
-else:
-    tps = [0, 45, 90, 180, 270]
+elif(timepoints == 5):
+    tps = [0, 15, 60, 180, 1440]
 
 #Set working directory
 #if gell run first
-if 'n' == 'y':
+if 'y' == 'y':
     os.chdir( r'C:\Users\natha\Desktop\CEMB Summer Program 2021\Discher Lab\Ex-vivo chick heart experiments\Experiments\Beating data_20210728\Data analysis\gels\{c}\{t}{h}'.format(c = con, h = hrt, t = treatment))
 else:
     os.chdir( r'C:\Users\natha\Desktop\CEMB Summer Program 2021\Discher Lab\Ex-vivo chick heart experiments\Experiments\Beating data_20210728\Data analysis\{c}\{t}{h}'.format(c = con, h = hrt, t = treatment))
@@ -45,40 +45,45 @@ else:
 
 
 #%%
-data_tp0 = pd.read_excel('{c}_{t}{h}_t{tp}_analysis.xlsx'.format(c = con, h = hrt, t = treatment, tp = tps[0]), usecols = "A:B")
-Time_tp0 = data_tp0['Time (s)']
-
-signal_tp0 = (data_tp0['delta AR/ARref'])
-# signal = sin(Time);   %%  Raw signal with time period of ~6.5 sec i.e. Freq = 1/6.5 = 0.15 Hz
-L = len(Time_tp0)
-Fs= L/max(Time_tp0)     #% Sampling frequency                    
-T = 1/Fs #             % Sampling period  
-
-
-Y_tp0 = fft(signal_tp0.to_numpy())         #Compute DFT of Signal and get real and imaginary components
-P2_tp0 = abs(Y_tp0/L)
-                                           #Whole spectrum
-P1_tp0 = 2*P2_tp0[:int(L/2)]
-P1_tp0[0] = P1_tp0[0]/2
-                                           #      % One-sided spectrum
-
-#P1_tp0(2:end-1) = 2*P1_tp0(2:end-1);              % Even-valued signal length L.
-
-#Filter heights less than this when doing peak to peak analysis
-filterfraction = 0.66;
-
-# Obtain peaks
-minimum_peakheight_tp0 = filterfraction*max(signal_tp0)
-peaks_tp0 = find_peaks(signal_tp0, height = minimum_peakheight_tp0)
-time_peaks_tp0 = Time_tp0[peaks_tp0[0]]
-
-# %%To get valleys, you invert the signal
-invertedsignal_tp0 = max(signal_tp0) - signal_tp0
-minimum_valleyheight_tp0 = filterfraction*max(invertedsignal_tp0);
-valleys_tp0 = find_peaks(invertedsignal_tp0, height = minimum_valleyheight_tp0)
-time_valleys_tp0 = Time_tp0[valleys_tp0[0]]
-Values_valleys_tp0 = max(signal_tp0)-valleys_tp0[1]['peak_heights']  #%%%  True valley values
-amp_tp0 = peaks_tp0[1]['peak_heights'].mean()-Values_valleys_tp0.mean()
+if os.path.exists('{c}_{t}{h}_t{tp}_analysis.xlsx'.format(c = con, h = hrt, t = treatment, tp = tps[0])):
+    data_tp0 = pd.read_excel('{c}_{t}{h}_t{tp}_analysis.xlsx'.format(c = con, h = hrt, t = treatment, tp = tps[0]), usecols = "A:B")
+    Time_tp0 = data_tp0['Time (s)']
+    
+    signal_tp0 = (data_tp0['delta AR/ARref'])
+    # signal = sin(Time);   %%  Raw signal with time period of ~6.5 sec i.e. Freq = 1/6.5 = 0.15 Hz
+    L = len(Time_tp0)
+    Fs= L/max(Time_tp0)     #% Sampling frequency                    
+    T = 1/Fs #             % Sampling period  
+    
+    
+    Y_tp0 = fft(signal_tp0.to_numpy())         #Compute DFT of Signal and get real and imaginary components
+    P2_tp0 = abs(Y_tp0/L)
+                                               #Whole spectrum
+    P1_tp0 = 2*P2_tp0[:int(L/2)]
+    P1_tp0[0] = P1_tp0[0]/2
+                                               #      % One-sided spectrum
+    
+    #P1_tp0(2:end-1) = 2*P1_tp0(2:end-1);              % Even-valued signal length L.
+    
+    #Filter heights less than this when doing peak to peak analysis
+    filterfraction = 0.66;
+    
+    # Obtain peaks
+    minimum_peakheight_tp0 = filterfraction*max(signal_tp0)
+    peaks_tp0 = find_peaks(signal_tp0, height = minimum_peakheight_tp0)
+    time_peaks_tp0 = Time_tp0[peaks_tp0[0]]
+    
+    # %%To get valleys, you invert the signal
+    invertedsignal_tp0 = max(signal_tp0) - signal_tp0
+    minimum_valleyheight_tp0 = filterfraction*max(invertedsignal_tp0);
+    valleys_tp0 = find_peaks(invertedsignal_tp0, height = minimum_valleyheight_tp0)
+    time_valleys_tp0 = Time_tp0[valleys_tp0[0]]
+    Values_valleys_tp0 = max(signal_tp0)-valleys_tp0[1]['peak_heights']  #%%%  True valley values
+    amp_tp0 = peaks_tp0[1]['peak_heights'].mean()-Values_valleys_tp0.mean()
+    
+else:
+    P1_tp0 = []
+    amp_tp0 = None
 
 #%%
 data_tp1 = pd.read_excel('{c}_{t}{h}_t{tp}_analysis.xlsx'.format(c = con, h = hrt, t = treatment, tp = tps[1]), usecols = "A:B")
@@ -161,7 +166,7 @@ amp_tp2 = peaks_tp2[1]['peak_heights'].mean()-Values_valleys_tp2.mean()
 
 
 #%%
-if(timepoints == 4):
+if(timepoints >= 4):
     data_tp3 = pd.read_excel('{c}_{t}{h}_t{tp}_analysis.xlsx'.format(c = con, h = hrt, t = treatment, tp = tps[3]), usecols = "A:B")
     Time_tp3 = data_tp3['Time (s)']
     
@@ -269,19 +274,20 @@ frequency_distribution.to_excel(excel_writer = writer, sheet_name = 'Freq_Distri
 summary =writer.sheets['Summary']
 freqDisSheet = writer.sheets['Freq_Distribution']
 
-#create chart object for strain over time
-strainOt = workbook.add_chart({'type': 'scatter','subtype': 'straight_with_markers'})
-strainOt.add_series({
-    'categories': ['Summary', 1, 1, len(tps)+1, 1],
-    'values':     ['Summary', 1, 2, len(tps)+1, 2],
-})
-
 #create chart object for frequency over time
 freqOt = workbook.add_chart({'type': 'scatter','subtype': 'straight_with_markers'})
 freqOt.add_series({
-    'categories': ['Summary', 1, 1, len(tps)+1, 1],
-    'values':     ['Summary', 1, 3, len(tps)+1, 3],
+    'categories': ['Summary', 1, 1, len(tps), 1],
+    'values':     ['Summary', 1, 2, len(tps), 2],
 })
+
+#create chart object for strain over time
+strainOt = workbook.add_chart({'type': 'scatter','subtype': 'straight_with_markers'})
+strainOt.add_series({
+    'categories': ['Summary', 1, 1, len(tps), 1],
+    'values':     ['Summary', 1, 3, len(tps), 3],
+})
+
 
 
 #create chart object for Frequency distribution
@@ -290,32 +296,39 @@ freqDis = workbook.add_chart({'type': 'scatter','subtype': 'straight'})
 #Add frequency distribution for tp0
 freqDis.add_series({
     'name':       'tp0',
-    'categories': ['Freq_Distribution', 1, 1, 1, len(f)+1],
-    'values':     ['Freq_Distribution', 2, 1, 2, len(P1_tp0)+1],
+    'categories': ['Freq_Distribution', 1, 1, 1, len(f)],
+    'values':     ['Freq_Distribution', 2, 1, 2, len(P1_tp0)],
 })
 
 #Add frequency distribution for tp1
 freqDis.add_series({
     'name':       'tp1',
-    'categories': ['Freq_Distribution', 1, 1, 1, len(f)+1],
-    'values':     ['Freq_Distribution', 3, 1, 3, len(P1_tp1)+1],
+    'categories': ['Freq_Distribution', 1, 1, 1, len(f)],
+    'values':     ['Freq_Distribution', 3, 1, 3, len(P1_tp1)],
 })
 
 #Add frequency distribution for tp2
 freqDis.add_series({
     'name':       'tp2',
-    'categories': ['Freq_Distribution', 1, 1, 1, len(f)+1],
-    'values':     ['Freq_Distribution', 4, 1, 4, len(P1_tp2)+1],
+    'categories': ['Freq_Distribution', 1, 1, 1, len(f)],
+    'values':     ['Freq_Distribution', 4, 1, 4, len(P1_tp2)],
 })
 
-if len(tps) ==4:
+if len(tps) >= 4:
 #Add frequency distribution for tp3
     freqDis.add_series({
         'name':       'tp3',
-        'categories': ['Freq_Distribution', 1, 1, 1, len(f)+1],
-        'values':     ['Freq_Distribution', 5, 1, 5, len(P1_tp3)+1],
+        'categories': ['Freq_Distribution', 1, 1, 1, len(f)],
+        'values':     ['Freq_Distribution', 5, 1, 5, len(P1_tp3)],
     })
 
+if len(tps) ==5:
+#Add frequency distribution for tp3
+    freqDis.add_series({
+        'name':       'tp4',
+        'categories': ['Freq_Distribution', 1, 1, 1, len(f)],
+        'values':     ['Freq_Distribution', 6, 1, 6, len(P1_tp3)],
+    })
 
 # Set name on axis of colChartBkwd_Fwd object and insert to 920vs860nm sheet
 freqDis.set_title({'name': 'Frequency Distribution of {c} {t}{h}'.format(c = con, h = hrt, t =treatment)})
@@ -326,7 +339,7 @@ freqDis.set_y_axis({'name': 'Magnitude','min': 0, 'max': 2,
 
 # Set name on axis of colChartBkwd_Fwd object and insert to 920vs860nm sheet
 strainOt.set_title({'name': 'Strain over time {c} {t}{h}'.format(c = con, h = hrt, t =treatment)})
-strainOt.set_x_axis({'name': 'Time (min)', 'min': 0, 'max': tps[-1]+15})
+strainOt.set_x_axis({'name': 'Time (min)', 'min': 0, 'max': tps[-1]+160})
 strainOt.set_y_axis({'name': 'AR/AR_ref','min': 0, 'max': 5,
                   'major_gridlines': {'visible': True},
 })
@@ -335,7 +348,7 @@ strainOt.set_legend({'none': True})
 
 # Set name on axis of colChartBkwd_Fwd object and insert to 920vs860nm sheet
 freqOt.set_title({'name': 'Frequency over time {c} {t}{h}'.format(c = con, h = hrt, t =treatment)})
-freqOt.set_x_axis({'name': 'Time (min)', 'min': 0, 'max': tps[-1]+15})
+freqOt.set_x_axis({'name': 'Time (min)', 'min': 0, 'max': tps[-1]+160})
 freqOt.set_y_axis({'name': 'Frequency (Hz)','min': 0, 'max': 6,
                   'major_gridlines': {'visible': True},
 })
@@ -343,8 +356,8 @@ freqOt.set_legend({'none': True})
 
 #Insert chartsheet
 freqDisSheet.insert_chart('G8', freqDis)
-summary.insert_chart('F1',strainOt)
-summary.insert_chart('F17',freqOt)
+summary.insert_chart('F1',freqOt)
+summary.insert_chart('F17',strainOt)
 
 #close excel file       
 writer.close()
